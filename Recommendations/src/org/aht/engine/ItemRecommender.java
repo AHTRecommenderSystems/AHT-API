7cb2c49735d358b587a6f32a72d2fe823b54d7a7
Azucena López <blankazucenalg@gmail.com>
2015-11-05 15:21:05 -0600

    Update query to search item like name

--- Recommendations/src/org/aht/engine/ItemRecommender.java
+++ Recommendations/src/org/aht/engine/ItemRecommender.java
@@ -7,10 +7,10 @@ import java.sql.*;
  */
 public class ItemRecommender {
 
-    public ResultSet getRecommendations(int dishID,Connection con){
+    public ResultSet getRecommendations(int dishID,int numberOfRecommendations, Connection con){
         ResultSet rs = null;
         try(Statement stmt = con.createStatement()){
-            rs = stmt.executeQuery("MATCH    (d1:Dish {dishID:"+dishID+"})-[s:SAME_CATEGORIES]-(d2:Dish) WITH     d2, s.similarity AS sim ORDER BY sim DESC LIMIT 15 RETURN   d2.dishID AS ID, d2.dishName AS Neighbor, sim AS Similarity;");
+            rs = stmt.executeQuery("MATCH    (d1:Dish {dishID:"+dishID+"})-[s:SAME_CATEGORIES]-(d2:Dish) WITH     d2, s.similarity AS sim ORDER BY sim DESC LIMIT "+numberOfRecommendations+" RETURN   d2.dishID AS ID, d2.dishName AS Neighbor, sim AS Similarity;");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -20,7 +20,7 @@ public class ItemRecommender {
     public ResultSet selectItem(String query, Connection con) throws SQLException {
         ResultSet rs = null;
         try(Statement stmt = con.createStatement()){
-            rs = stmt.executeQuery("MATCH (d:Dish {dishName:'"+query+"'}) return d.dishID AS ID,d.dishName AS Name;");
+            rs = stmt.executeQuery("MATCH (d:Dish) WHERE d.dishName =~ '.*"+query+".*' return d.dishID AS ID,d.dishName AS Name;");
         } catch (SQLException e) {
             e.printStackTrace();
         }
