ab1ca4baf3901575301c9ddaf2c855f803fbe1e8
Azucena López <blankazucenalg@gmail.com>
2015-11-05 23:39:36 -0600

    Resultset iteration issue solved

--- Recommendations/src/org/aht/test/Main.java
+++ Recommendations/src/org/aht/test/Main.java
@@ -16,47 +16,56 @@ import java.sql.SQLException;
  * Created by azu on 30/10/15.
  */
 public class Main {
-    public static void main(String args[]) throws ClassNotFoundException, SQLException, IOException {
+    public static void main(String args[]){
         // Database info
         String username="neo4j";
         String password="n0m3l0s3";
         String categorias = "file:///home/azu/Proyectos/AHTRecommenderSystems/AHT-Docs/Data/categories.csv";
         String platillos = "file:///home/azu/Proyectos/AHTRecommenderSystems/AHT-Docs/Data/dishes.csv";
         String dishes_categories = "file:///home/azu/Proyectos/AHTRecommenderSystems/AHT-Docs/Data/dish_has_category.csv";
-        // Connect
-        Connection con = DataImport.connectToNeo4j(username,password);
+        try {
+            // Connect
+            Connection con = DataImport.connectToNeo4j(username, password);
 
-        //Create Data
-        DataImport di = new DataImport();
-        //di.importCategories(categorias,con);
-        //di.importDishes(platillos,con);
-        //di.createRelationshipsDishesAndCategories(dishes_categories,con);
-        //di.createSimilarityDishes(con);
+            //Create Data
+            DataImport di = new DataImport();
+            //di.importCategories(categorias,con);
+            //di.importDishes(platillos,con);
+            //di.createRelationshipsDishesAndCategories(dishes_categories,con);
+            //di.createSimilarityDishes(con);
 
-        //Get recommendation forn an item
-        ItemRecommender ir = new ItemRecommender();
-        String query = "Merengues argentinos"; //Replace with the name of another dish to get a recommendation
+            //Get recommendation forn an item
+            String query = "ensalada"; //Replace with the name of another dish to get a recommendation
+            int numberOfRecommendations = 15;
 
-        ResultSet is = ir.selectItem(query,con); //Look for the dish
-        is.next();
-        int dishID = is.getInt("ID");
+            ItemRecommender ir = new ItemRecommender();
+            ResultSet is = ir.selectItem(query, con); //Look for the dish
+            if(is.next()) {
+                int dishID = is.getInt("ID");
 
-        ResultSet rs = ir.getRecommendations(dishID,con); //Get recommendations for the dish. OR, you can replace ID to get recommendations of another dish
-        File recommendations = new File("recommendations.csv");
-        FileWriter fw = new FileWriter(recommendations);
-        PrintWriter pw = new PrintWriter(fw);
+                ResultSet rs = ir.getRecommendations(dishID, numberOfRecommendations, con); //Get recommendations for the dish. OR, you can replace ID to get recommendations of another dish
+                File recommendations = new File("recommendations.csv");
+                FileWriter fw = new FileWriter(recommendations);
+                PrintWriter pw = new PrintWriter(fw);
 
-        pw.println("Recomendaciones para " + query);
-        System.out.println("Recomendaciones para " + query);
-        while(rs.next()) {
-            //Retrieve by column name
-            String dish = rs.getString("Neighbor");
-            int id = rs.getInt("ID");
-            pw.println(id + ", " + dish);
-            System.out.println(id+"\t"+ dish);
-
+                pw.println("Recomendaciones para " + query);
+                System.out.println("Recomendaciones para " + query);
+                pw.println(dishID + " " + is.getString("Name"));
+                System.out.println(dishID + " " + is.getString("Name"));
+                int i = 0;
+                while (i < numberOfRecommendations && rs.next()) {
+                    //Retrieve by column name
+                    String dish = rs.getString("Neighbor");
+                    int id = rs.getInt("ID");
+                    pw.println(id + ", " + dish);
+                    System.out.println(id + "\t" + dish);
+                    i++;
+                }
+                fw.close();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
-        fw.close();
 
     }
 }
