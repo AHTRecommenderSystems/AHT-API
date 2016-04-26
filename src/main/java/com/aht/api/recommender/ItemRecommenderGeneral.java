b5dbf8d7ec2098fe75b37258db566976e64202c5
Blanca Azucena López Garduño <blankazucenalg@gmail.com>
2016-04-25 23:33:17 -0500

    Update item recommender general, using Manhattan lenght

--- src/main/java/com/aht/api/recommender/ItemRecommenderGeneral.java
+++ src/main/java/com/aht/api/recommender/ItemRecommenderGeneral.java
@@ -2,15 +2,21 @@ package com.aht.api.recommender;
 
 import java.util.Set;
 import java.util.HashSet;
+
+import com.aht.api.evaluator.ManhattanLength;
 import com.aht.api.model.node.Item;
 import com.aht.api.model.node.User;
 import com.aht.api.model.node.Characteristic;
 
 public class ItemRecommenderGeneral implements ItemRecommender {
+	ManhattanLength evaluator = new ManhattanLength();
 
 	public Set<Item> getTopNRecommendationByItem(Item item, int N) {
 		Set<Item> topNRecommendations = new HashSet<Item>();
 		for (Characteristic characteristic : item.getModelCharacteristics()) {
+			for(Item neighbor: characteristic.getItems()){
+				double value = evaluator.getEvaluationForItems(item, neighbor);
+			}
 			System.out.println(characteristic.getName() + "\t" + characteristic.getItems());
 		}
 		return topNRecommendations;
