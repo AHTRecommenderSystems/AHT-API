b1dc58b0d8140610cacae80f00e98a6a47b83d52
Blanca Azucena López Garduño <blankazucenalg@gmail.com>
2016-04-27 22:55:19 -0500

    getTopNRecommendationsByItem developed

--- src/main/java/com/aht/api/recommender/ItemRecommenderCalculatedSimilitude.java
+++ src/main/java/com/aht/api/recommender/ItemRecommenderCalculatedSimilitude.java
@@ -1,7 +1,7 @@
 package com.aht.api.recommender;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.text.Collator;
+import java.util.*;
 
 import com.aht.api.evaluator.Evaluator;
 import com.aht.api.evaluator.ManhattanLength;
@@ -10,14 +10,28 @@ import com.aht.api.model.node.User;
 import com.aht.api.model.node.Characteristic;
 
 public class ItemRecommenderCalculatedSimilitude implements ItemRecommender {
-	private Evaluator evaluator = new ManhattanLength();
+	private ManhattanLength evaluator = new ManhattanLength();
 	public List<Item> getTopNRecommendationByItem(Item item, int N) {
-		List<Item> topNRecommendations = new LinkedList<Item>();
+		List<Wrapper> wrappedRecommendations = new ArrayList<Wrapper>();
 		for (Characteristic characteristic : item.getModelCharacteristics()) {
 			for(Item neighbor: characteristic.getModelItems()){
 				double value = evaluator.getEvaluationForItems(item, neighbor);
+				Wrapper wrapper = new Wrapper(neighbor, value);
+				wrappedRecommendations.add(wrapper);
 			}
-			System.out.println(characteristic + "\t" + characteristic.getModelItems());
+		}
+		Collections.sort(wrappedRecommendations, new Comparator<Wrapper>() {
+			public int compare(Wrapper wrapper1, Wrapper wrapper2)
+			{
+				if (wrapper1.getValue() < wrapper2.getValue()) return -1;
+				if (wrapper1.getValue() > wrapper2.getValue()) return 1;
+				return 0;
+			}
+		});
+
+		List topNRecommendations = new LinkedList<>();
+		for(Wrapper wrapper : wrappedRecommendations){
+			topNRecommendations.add(wrapper.getItem());
 		}
 		return topNRecommendations;
 	}
@@ -37,4 +51,30 @@ public class ItemRecommenderCalculatedSimilitude implements ItemRecommender {
 	public Item getUniqueRecommendationByUser(User user) {
 		  return null;
 	  }
+
+	private class Wrapper {
+		private double value;
+		private Item item;
+
+		public Wrapper(Item item, double value) {
+			this.item = item;
+			this.value = value;
+		}
+
+		public double getValue() {
+			return value;
+		}
+
+		public void setValue(double value) {
+			this.value = value;
+		}
+
+		public Item getItem() {
+			return item;
+		}
+
+		public void setItem(Item item) {
+			this.item = item;
+		}
+	}
 }
